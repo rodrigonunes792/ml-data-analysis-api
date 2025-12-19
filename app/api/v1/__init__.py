099be5504f229145477de03369392a3a38ffbd77
Rodrigo Nunes <46222357+rodrigonunes792@users.noreply.github.com>
2025-12-19 07:02:12 -0300

    Docs: Add __all__ export declaration to API router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,3 +5,5 @@ api_router = APIRouter()
 
 api_router.include_router(analysis.router, prefix="/analysis", tags=["EDA"])
 api_router.include_router(ml.router, prefix="/ml", tags=["Machine Learning"])
+
+__all__ = ["api_router"]
